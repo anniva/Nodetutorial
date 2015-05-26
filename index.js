0d71ed370636676cbcf103ccb171ff0d3594aa98
Anni Vaananen <anni.vaananen@gmail.com>
2015-05-26 14:05:19 +0100

    fixi

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var ac = {};
 
 ac.import = function(callback){ 	// assigning a fuction to a property > so that it can be used later on
 	var filename = __dirname + '/words.txt';
-	fs.readFile(filename, 'utf8', function(err, data){	// utf8 -> gives the data in a string so you dont need to convert it to a string
+	fs.readFile(filename, 'utf8', function (err, data){	// utf8 -> gives the data in a string so you dont need to convert it to a string
 	 	if(err){	
 		console.log(err);
 		}
-		ac.words = data.split('\n');
 
-		callback(ac.words);
+		ac.words = data.split('\n');
+		console.log(data);
+		return callback(ac.words);
 		});
 }
 
